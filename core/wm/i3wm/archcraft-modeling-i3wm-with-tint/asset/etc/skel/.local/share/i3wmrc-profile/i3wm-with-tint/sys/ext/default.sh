96052f9935ca2c99fa60a212410ff55b0c7264b2
samwhelp <samwhelp@users.noreply.github.com>
2021-10-16 16:01:17 +0800

    fix

--- core/wm/i3wm/archcraft-modeling-i3wm-with-tint/asset/etc/skel/.local/share/i3wmrc-profile/i3wm-with-tint/sys/ext/default.sh
+++ core/wm/i3wm/archcraft-modeling-i3wm-with-tint/asset/etc/skel/.local/share/i3wmrc-profile/i3wm-with-tint/sys/ext/default.sh
@@ -42,17 +42,8 @@ default_keybind () {
 
 default_theme () {
 	# theme
-	#bspc monitor -d I II III IV V VI VII VIII IX X
-	#bspc monitor -d 1 2 3 4 5 6 7 8 9 10
-	bspc monitor -d Term Edit Web File Misc
-	#bspc monitor -d 1 2 3 4 5
-
-	bspc config border_width         2
-	bspc config window_gap          12
-
-	bspc config split_ratio          0.52
-	bspc config borderless_monocle   true
-	bspc config gapless_monocle      true
+	
+	return 0
 }
 
 
